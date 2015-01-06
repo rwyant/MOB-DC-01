47b0d8c622bee0c471bd91d50f59b885faf64b76
Rob Wyant <rob@getyapper.com>
2015-01-06 12:21:55 -0500

    Updates to Lecture 4 playground.

--- Classwork/Lecture 4/Lesson4Playground.playground/section-1.swift
+++ Classwork/Lecture 4/Lesson4Playground.playground/section-1.swift
@@ -1,3 +1,9 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
 // Lesson 04 playground
 
 import Foundation
@@ -26,34 +32,46 @@ if age>21 {
 
 
 // TODO: Print “you can drive” if the user is above 16 but below 18. It should print “You can drive and vote” if the user is above 18 but below 21. If the user is above 21, it should print “you can drive, vote and drink (but not at the same time!”.
-if age>21 {
-    println("You can drive, vote, and drink (but not at the same time!")
-} else if age>18 {
-    println("You can drive and vote")
-} else if age>16 {
-    println("You can drive")
-}else {
-    println("You are too young")
-}
 
+if age < 16 {
+    println("You are too young")
+} else if age < 18 {
+    println("You can drive")
+} else if age < 21 {
+    println("You can drive and vote")
+} else {
+    println("You can drive, vote and drink (but not at the same time)!")
+}
 
 // TODO: Print the first fifty multiples of seven minus one (e.g. the first three multiples are 7, 14, 21. The first three multiples minus one are 6, 13, 20)
 
 
 for multiples in 1...50 {
-        println(-1 + multiples*7)
+    println(-1 + multiples*7)
 }
+
+//--or--
+
+//var x = 7
+//var y = 6
+//
+//for i in 1...49 {
+//    println(y)
+//    y = y + x
+//}
+//println(y)
+
 
 
 // TODO: Create a constant called number
-let number = 2
+let number = 4
 println(number)
 
 // TODO: Print whether the above number is even
-if (number % 100 == 2) {
-    println("Even")
+if (number % 2) == 1 {
+    println("The number is odd")
 } else {
-    println("Odd")
+    println("The number is even")
 }
 
 // TODO: The first fibonacci number is 0, the second is 1, the third is 1, the fourth is two, the fifth is 3, the sixth is 5, etc. The Xth fibonacci number is the sum of the X-1th fibonacci number and the X-2th fibonacci number. Print the 37th fibonacci number below
@@ -67,7 +85,6 @@ for index in 1...35 {
     three = one + two
 }
 println(three)
-
 
 
 
@@ -99,7 +116,7 @@ while rdw < 101 {
     } else {
         println(rdw)
     }
-        ++rdw
+    ++rdw
 }
 
 
