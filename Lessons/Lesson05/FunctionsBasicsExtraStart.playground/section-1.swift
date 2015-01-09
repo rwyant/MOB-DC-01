43a6e4e657fc2c927860cd33839b46916e6ec0fd
Rob Wyant <rob@getyapper.com>
2015-01-09 15:45:14 -0500

    Deleting my work in 'lessons' files so there won't be any conflict when I push to origin.

--- Lessons/Lesson05/FunctionsBasicsExtraStart.playground/section-1.swift
+++ Lessons/Lesson05/FunctionsBasicsExtraStart.playground/section-1.swift
@@ -61,33 +61,18 @@ Final note: don't be afraid to have long function names, they'll help you in big
 
 // Create a function knockknock that just prints the line "Who's there?"
 // This function has no parameters nor a return value
-func knockknock() {
-    println("Who's There")
-}
-knockknock()
 
 
 // That was fun! Let's joke around and knock a few times more
 // Write a function knockMultipleTimes that accepts one paramter
 // That parameter will be an int that let's us specify how many times we want to knock
 // Within the function we have a loop that runs x ammount of times that calls the knock knock function
-func knockMultipleTimes(amountOfTimes: Int) {
-    for index in 1...amountOfTimes {
-    knockknock()
-}
-knockMultipleTimes(4)
-
 
 
 // HAHAHA, okay let's answer before the person behind the door goes nuts!
 // We will write a replyWhoIsHere function that accepts 2 parameters
 // Parameter one will be a firstname (String), and parameter two will be the homeCountry (also String)
 // The function will just print "It's me, {firstname}, from {homeCountry}"
-    func replyWhoIsHere(firstName:String, homeCountry:String) {
-        println("It's me, \(firstName), from \(homeCountry)")
-}
-replyWhoIsHere("Rob","USA")
-
 
 
 // Cool that worked! Let's say we want to reply with our age instead of home country!
