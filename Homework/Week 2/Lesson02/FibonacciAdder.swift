1f8eb3056d03fc469bb11acef9f4b507bafd7ee1
Rob Wyant <rob@getyapper.com>
2015-01-15 17:22:51 -0500

    HW 2: --Fourth View Controller (ToDo seven) completed

--- Homework/Week 2/Lesson02/FibonacciAdder.swift
+++ Homework/Week 2/Lesson02/FibonacciAdder.swift
@@ -1,30 +1,30 @@
 //
-//  FourthViewController.swift
+//  FibonacciAdder.swift
 //  Lesson02
 //
-//  Created by Rudd Taylor on 9/28/14.
-//  Copyright (c) 2014 General Assembly. All rights reserved.
+//  Created by Rob Wyant on 1/15/15.
+//  Copyright (c) 2015 General Assembly. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class FourthViewController: UIViewController {
-    @IBOutlet weak var fibNumText: UITextField!
-    @IBOutlet weak var fibLabel: UILabel!
-
+class FibonacciAdder {
     
     /*
     TODO seven: Hook up the text input box, label and and a ‘calculate’ button. Create a ‘fibonacci adder’ class with a method ‘fibonacciNumberAtIndex' which takes indexOfFibonacciNumber (an integer).  When the button is pressed, create an instance of that class, call the method, and print out the appropriate fibonacci number of an inputted integer.
     The first fibonacci number is 0, the second is 1, the third is 1, the fourth is two, the fifth is 3, the sixth is 5, etc. The Xth fibonacci number is the sum of the X-1th fibonacci number and the X-2th fibonacci number.*/
-    
-    @IBAction func fibButton(sender: AnyObject) {
-        var fibNumb:Int = fibNumText.text.toInt()!
-        var myFibClass = FibonacciAdder()
-        var result = myFibClass.fibonacciNumberAtIndex(fibNumb)
-        fibLabel.text = String(result)
+    func fibonacciNumberAtIndex(indexOfFibonacciNumber:Int)-> Int {
+        var fibNum = indexOfFibonacciNumber, current = 0, next = 1, result = 0
+        for index in 0..<fibNum {
+            let tempVar = current
+            current = next
+            next = tempVar + current
+            result = tempVar
+        }
+        
+        return result
+
+        
     }
-    
-    
-    
     
 }
