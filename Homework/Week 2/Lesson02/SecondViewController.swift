d0c44542ae2e6b58ecca234d10c2af96f9506b1f
Rob Wyant <rob@getyapper.com>
2015-01-15 14:59:44 -0500

    Committing HW Week 2: --View Controller 2 (Incomplete, need to convert String -> Int)

--- Homework/Week 2/Lesson02/SecondViewController.swift
+++ Homework/Week 2/Lesson02/SecondViewController.swift
@@ -9,6 +9,27 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    @IBOutlet weak var numText: UITextField!
+    @IBOutlet weak var sumLabel: UILabel!
 
     //TODO five: Display the cumulative sum of all numbers added every time the ‘add’ button is pressed. Hook up the label, text box and button to make this work.
+//    @IBAction func addButton(sender: AnyObject) {
+//        var num = 0
+//        var sum = 0
+//        func sumOfNumb() -> String {
+//        num = Int(numText.text)
+//        sum += num
+//        return String(sum)
+//        }
+//        
+//        sumLabel.text = sumOfNumb()
+//    }
+
+    
+
+
+
 }
+
+
+
