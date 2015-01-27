89913ede2b04484d743f5ec8a521b4cc2705b3e6
Rob Wyant <rob@getyapper.com>
2015-01-26 19:02:54 -0500

    Adding 'Tables and Segues' file

---
+++ Classwork/Lecture 9/Tables and Segues/Tables and Segues/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Tables and Segues
+//
+//  Created by Rob Wyant on 1/26/15.
+//  Copyright (c) 2015 Rob Wyant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var swipeView: UIView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
